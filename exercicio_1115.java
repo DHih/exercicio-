05c98785a7a8a347452dab8055a315b32e9d797a
DHih <57665806+DHih@users.noreply.github.com>
2020-03-20 22:44:48 -0300

    utilizando sistema cartesiano

--- exercicio_1115.java
+++ exercicio_1115.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
 //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano.
-	//Para cada ponto escrever o quadrante a que ele pertence. O algoritmo ser· encerrado quando pelo menos uma de duas
-	//coordenadas for NULA (nesta situaÁ„o sem escrever mensagem alguma).
+	//Para cada ponto escrever o quadrante a que ele pertence. O algoritmo ser√° encerrado quando pelo menos uma de duas
+	//coordenadas for NULA (nesta situa√ß√£o sem escrever mensagem alguma).
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
